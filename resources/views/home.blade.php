0b8afb91078112cd91319499bca396a0881e55ea
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2025-09-06 17:23:14 +0000

    6/9/2025

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,36 @@
     <title>Document</title>
 </head>
 <body>
-     <h1>Greatness</h1>
+
+    @auth
+    <p>Congrats you are logged in</p>
+    <form action='/logout' method = 'POST'>
+        @csrf
+        <button>Log out</button>
+    </form>
+    @else
+    <div style="border: 3px solid black;">
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input type="text" placeholder="name" name="name"/>
+            <input type="text" placeholder="email" name="email"/>
+            <input type="password" placeholder="password" name="password"/>
+            <button>Register</button>
+        </form>
+     </div>  
+
+     <div style="border: 3px solid black;">
+        <h2>Log in</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input type="text" placeholder="name" name="loginname"/>
+            <input type="password" placeholder="password" name="loginpassword"/>
+            <button>Log in</button>
+        </form>
+     </div>  
+    @endauth
+
+      
 </body>
 </html>
